ed72e9ff8340e02ce968193816eb088d7b297d79
bekhzodbek231 <javascriptdeveloper231@gmail.com>
2023-02-12 06:04:45 +0900

    some bugs have been debuged

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
 import './App.css';
 import HomeScreen from "./HomeScreen";
-import  { useEffect } from "react"
+import { useEffect } from "react"
 import LoginScreen from "./LoginScreen";
 import ProfileScreen from "./ProfileScreen";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { auth } from './firebase';
-import { useDispatch, useSelector }from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { login, logout, selectUser } from "./features/userSlice"
 
 
 function App() {
- 
+
   const user = useSelector(selectUser);
- const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
-useEffect(() => { 
-  const unsubscribe = auth.onAuthStateChanged((Authuser) => {
-    if (Authuser){
-      dispatch(login({
-        uid: Authuser.uid,
-        email: Authuser.email
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((Authuser) => {
+      if (Authuser) {
+        dispatch(login({
+          uid: Authuser.uid,
+          email: Authuser.email
+        }
+        ));
       }
-      ));
-    }
-    else{
-      dispatch(logout())
-    }
-   });
-   return unsubscribe;
-},[]);
+      else {
+        dispatch(logout())
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
 
-   
 
-  
+
+
   return (
-    <div className="app">   
-        {!user ? (
-          <LoginScreen />
-        ) : (    
-       <Routes>
-        <Route exact path='/' element={<HomeScreen />}/>
-        <Route path='profilescreen' element={<ProfileScreen />}/>
-        
-      </Routes>
-        )} 
-  
+    <div className="app">
+      {!user ? (
+        <LoginScreen />
+      ) : (
+        <Routes>
+          <Route exact path='/' element={<HomeScreen />} />
+          <Route path='profilescreen' element={<ProfileScreen />} />
+
+        </Routes>
+      )}
+
     </div>
   );
 }
